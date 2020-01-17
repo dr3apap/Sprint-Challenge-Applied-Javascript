b588c490286ce78f52a15f8c68b5523221d3f404
Adebola  Adesina <adebolaadesina@adebolas-mbp.home>
2020-01-17 10:19:49 -0800

    tab done and ready

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,20 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function createTabComponent(arr) {
+    const tabDiv = document.createElement('div')
+    const textHolder = document.createElement('p')
+    textHolder.textContent = arr
+    tabDiv.append(textHolder)
+    tabDiv.classList.add('tab')
+    console.log(tabDiv)
+
+    tabDiv.addEventListener("click", event => {
+        tabDiv.classList.toggle('active-tab');
+    })
+    return tabDiv
+}
+
+const entry = document.querySelector('.topics')
+console.log(entry)
+
 axios.get("https://lambda-times-backend.herokuapp.com/topics")
     .then(res => {
-
+        res.data.topics.forEach(item => {
+            entry.append(createTabComponent(item))
+        })
         console.log('The data returned:', res)
 
     })
     .catch(err => {
     })
-
-
-
-
-function createTabComponent() {
-    const tabDiv = document.createElement('div')
-    tabDiv.classList.add('tab')
-
-}
